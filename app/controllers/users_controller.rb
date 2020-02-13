aaeee5272393fa7aa0087a874596e52e06a4dcf6
thethinh <nguyenthethinh16011998@Gmail.com>
2020-02-14 00:16:33 +0700

    upload image complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,12 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :show, :destory]
 
   def index
-    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+    @users = User.paginate(page: params[:page], per_page: 7)
   end
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless FILL_IN
+    @microposts = @user.microposts.paginate(page: params[:page], per_page: 10)
   end
 
   def new
