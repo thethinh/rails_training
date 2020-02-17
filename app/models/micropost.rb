461d3395bfcf0003285231f3abd734906496474e
thethinh <nguyenthethinh16011998@Gmail.com>
2020-02-17 14:08:06 +0700

    change

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   mount_uploader :picture, PictureUploader
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validate  :picture_size
